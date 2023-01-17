38c894c34eb1c4928f236540491119e75d6379d1
iboffa <i.boffa@hotmail.it>
2023-01-17 11:01:28 +0200

    box detail component created

---
+++ src/app/box-detail/box-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-box-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './box-detail.component.html',
+  styleUrls: ['./box-detail.component.scss']
+})
+export class BoxDetailComponent {
+
+}
