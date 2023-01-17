98b2665604ae2797509b283bea637ee580f7f41d
iboffa <i.boffa@hotmail.it>
2023-01-17 10:59:09 +0200

    box list component created

---
+++ src/app/box-list/box-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-box-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './box-list.component.html',
+  styleUrls: ['./box-list.component.scss']
+})
+export class BoxListComponent {
+
+}
